c00121af5ad6e4d7c476da3efd807bc6f5be4e39
kogawa0301 <rstoyo3.kogawa.kazuma@techcamp-training.com>
2024-05-08 18:26:35 +0900

    Update messages_controller.rb

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,46 @@
+<<<<<<< 画像投稿機能の実装
+=======
+class MessagesController < ApplicationController
+
+  def index
+<<<<<<< Updated upstream
+     @message = Message.new
+     @room = Room.find(params[:room_id])
+     @messages = @room.messages.includes(:user)
+   end
+ 
+   def create
+     @room = Room.find(params[:room_id])
+     @message = @room.messages.new(message_params)
+     if @message.save
+       redirect_to room_messages_path(@room)
+     else
+       @messages = @room.messages.includes(:user)
+       render :index, status: :unprocessable_entity
+     end
+   end
+ 
+   private
+ 
+   def message_params
+     params.require(:message).permit(:content).merge(user_id: current_user.id)
+   end
+ end
+ 
+ 
+=======
+  def message_params
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+  end
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    @message.save
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
