09a7b5267181be6456bb7f0004a5ba81f821a924
kogawa0301 <rstoyo3.kogawa.kazuma@techcamp-training.com>
2024-05-08 18:26:54 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,14 +12,7 @@ module ChatApp
     config.load_defaults 7.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
-    
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
