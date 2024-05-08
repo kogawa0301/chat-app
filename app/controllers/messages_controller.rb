d7d04c4313e4b16d44f5b2fb3aba6c22ae5cf3fd
kogawa0301 <rstoyo3.kogawa.kazuma@techcamp-training.com>
2024-05-08 18:13:52 +0900

    z

--- app/controllers/messages_controller.rb
+++
@@ -1,7 +0,0 @@
-class MessagesController < ApplicationController
-  def index
-  end
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
-  end
-end
